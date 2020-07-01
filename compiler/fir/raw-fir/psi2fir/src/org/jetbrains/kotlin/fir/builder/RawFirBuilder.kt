a4a250f9553a5fd12e43a81e3c8793475716c075
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2020-07-01 16:43:35 +0200

    Add FirScript element and appropriate infrastructure

--- compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/raw-fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -586,6 +586,7 @@ class RawFirBuilder(
                 source = file.toFirSourceElement()
                 session = baseSession
                 origin = FirDeclarationOrigin.Source
+                val n: String = file.name
                 name = file.name
                 packageFqName = context.packageFqName
                 for (annotationEntry in file.annotationEntries) {
@@ -601,6 +602,25 @@ class RawFirBuilder(
                 }
                 for (declaration in file.declarations) {
                     declarations += declaration.convert<FirDeclaration>()
+                }
+            }
+        }
+
+        override fun visitScript(script: KtScript, data: Unit?): FirElement {
+            context.packageFqName = script.containingKtFile.packageFqName
+            return buildScript {
+                source = script.toFirSourceElement()
+                session = baseSession
+                origin = FirDeclarationOrigin.Source
+                name = script.fqName.shortName()
+                returnTypeRef = implicitUnitType
+                symbol = FirScriptSymbol(name)
+                for (declaration in script.declarations) {
+                    if (declaration is KtDestructuringDeclaration) {
+                        // TODO!
+                    } else {
+                        declarations += declaration.convert<FirDeclaration>()
+                    }
                 }
             }
         }
